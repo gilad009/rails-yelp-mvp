b015a30df95aa13f23418105504df8ed3f7d41e8
Gilad Milashut <gilad.milashut@gmail.com>
2023-05-25 11:00:22 +0200

    Article Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+5.times do
+  restaurants = Restaurant.new(
+  name: "Resto Resto",
+  address: "Berlin",
+  phone_number: "030",
+  category: "chinese"
+  )
+  restaurants.save!
+end
